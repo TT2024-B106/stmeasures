22236aaf80a24cde1cbeb3b3fbac94692966b7ba
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-10-01 19:50:25 -0600

    Fixed test and added pytest

--- tests/test_euclidean.py
+++ tests/test_euclidean.py
@@ -1,4 +1,14 @@
-from stmeasures.calculate import euclidean
+import pytest
+from stmeasures.calculate.euclidean import Euclidean
 
-def test_basic():
-    assert euclidean.distance([1, 2], [3, 4]) == 2.8284271247461903
+@pytest.fixture(scope="module")
+def euclidean():
+    instance = Euclidean()
+
+    assert instance.lib is not None
+
+    return instance
+
+def test_basic(euclidean):
+    approx_val = 2.8284271247461903
+    assert euclidean.distance([1, 2], [3, 4]) == pytest.approx(approx_val)
