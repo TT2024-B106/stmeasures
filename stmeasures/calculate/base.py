be286c94905c690ab7fa0535bd5aa79bed062d1e
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-10-01 19:34:15 -0600

    Implemented basic hierarchy

---
+++ stmeasures/calculate/base.py
@@ -0,0 +1,33 @@
+import ctypes
+
+from stmeasures import _libpath
+
+class BaseAlgorithm():
+    def __init__(self, libname: str) -> None:
+        self._libpath = _libpath(libname)
+        self._lib = None
+
+        self._load_library()
+
+    @property
+    def lib(self) -> ctypes.CDLL:
+        if hasattr(self, '_lib') and self._lib:
+            return self._lib
+        else:
+            raise RuntimeError(
+                f"Shared library '{self._libpath}' is not loaded"
+            )
+
+    def _load_library(self) -> None:
+        try:
+            self._lib = ctypes.CDLL(self._libpath)
+        except OSError as ose:
+            raise RuntimeError(
+                f"Failed to load the shared library '{self._libpath}':\n"
+                + f"{ose}"
+            )
+        except Exception as e:
+            raise RuntimeError(
+                f"An unexpected error occurred while loading '{self._libpath}'"
+                + f":\n{e}"
+            )
--- stmeasures/calculate/euclidean.py
+++ stmeasures/calculate/euclidean.py
@@ -1,19 +1,26 @@
 import ctypes
 
-from stmeasures import _libpath
+from stmeasures.calculate.base import BaseAlgorithm
 
-_lib = ctypes.CDLL(_libpath("libeuclidean"))
+class Euclidean(BaseAlgorithm):
+    def __init__(self, libname="libeuclidean") -> None:
+        super().__init__(libname)
 
-def distance(p: list[float], q: list[float]) -> float:
-    len_p, len_q = len(p), len(q)
+    def distance(
+            self,
+            p: list[float],
+            q: list[float]
+        ) -> float:
+        len_p = len(p)
 
-    if not p or not q:
-        raise ValueError("One or both arrays are empty")
-    if len_p != len_q:
-        raise ValueError(f"Arrays are not the same size\n{p}\n{q}")
+        # TODO: Validation in Base class?
 
-    doublearray = ctypes.c_double * len_p
-    _lib.distance.argtypes = [doublearray, doublearray, ctypes.c_size_t]
-    _lib.distance.restype = ctypes.c_double
+        doublearray = ctypes.c_double * len_p
+        self.lib.distance.argtypes = [
+            doublearray,
+            doublearray,
+            ctypes.c_size_t,
+        ]
+        self.lib.distance.restype = ctypes.c_double
 
-    return _lib.distance(doublearray(*p), doublearray(*q), len_p)
+        return self.lib.distance(doublearray(*p), doublearray(*q), len_p)
