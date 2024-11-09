e4419c700a8ddf1d2c005b9f7aff16da3c3bd7d7
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-11-08 22:31:32 -0600

    Changed lcss.distance args, added validations and deprecated original method

--- tests/test_lcss.py
+++ tests/test_lcss.py
@@ -10,8 +10,8 @@ def lcss():
 def test_lib(lcss):
     assert lcss.lib is not None
 
-def test_ers(lcss):
-    r = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    s = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+def test_lcss(lcss):
+    r = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
+    s = [(11, 12), (13, 14), (15, 16), (17, 18), (19, 20)]
 
     assert lcss.distance(r, s) == approx(0.1)
