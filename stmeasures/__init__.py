e43e635955d53279ef3b7eb37d4f04fd4efdd8dd
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-11-11 22:55:29 -0600

    Changed root __init__ so it import main modules

--- stmeasures/__init__.py
+++ stmeasures/__init__.py
@@ -1,8 +1,3 @@
 """Similar Trajectories Measurements."""
 
-import os
-
-def _libpath(libname: str):
-    clibpath = f"../stmeasures-clib/{libname}.so"
-    pkgpath = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(pkgpath, clibpath)
+from stmeasures.calculator import *
