892debb87da43d5dfba8e4f118a09ad0fb9d6041
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-11-13 20:45:36 -0600

    Merged stmeasures/__init__.py from calculator Removed calculates module and added main modules from this branch

--- stmeasures/__init__.py
+++ stmeasures/__init__.py
@@ -1,8 +1,4 @@
 """Similar Trajectories Measurements."""
 
-import os
-
-def _libpath(libname: str):
-    clibpath = f"../stmeasures-clib/{libname}.so"
-    pkgpath = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(pkgpath, clibpath)
+from stmeasures.read import *
+from stmeasures.visualize import *
